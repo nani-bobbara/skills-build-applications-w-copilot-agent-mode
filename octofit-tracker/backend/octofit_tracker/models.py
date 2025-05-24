03661c2dc96691b6b51d353d1397d2166c9c4b9c
nani-bobbara <44444804+nani-bobbara@users.noreply.github.com>
2025-05-24 02:29:43 +0000

    Fix Team model: use ManyToManyField for members to support Djongo/MongoDB

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -11,7 +11,7 @@ class User(models.Model):
 class Team(models.Model):
     _id = models.ObjectIdField()
     name = models.CharField(max_length=100)
-    members = models.ArrayField(model_container=User)
+    members = models.ManyToManyField('User', related_name='teams')
     def __str__(self):
         return self.name
 
